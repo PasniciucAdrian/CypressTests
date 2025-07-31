9c51af8335357af6c96e1ff3beeec260511bd197
AdrianPasniciuc\Adrian Pasniciuc <pasniciuc.adrian09@gmail.com>
2025-07-31 12:49:46 +0300

    Initial project with updated structure and README

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://www.cypress.io',
+    experimentalOriginDependencies: true,
+    setupNodeEvents(on, config) {
+      // plugins - we don't use any for now
+    }
+  }
+});
